7fa4c0aab5a181995f31ffe44a1bdccd74def368
danish45007 <danish45007@gmail.com>
2020-08-07 03:06:29 +0530

    context-api set-up

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { DataLayer } from './DataLayer';
+import reducer, { initalState } from './reducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initalState={initalState} reducer={reducer}>
+      <App />
+    </DataLayer>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
---
+++ src/reducer.js
@@ -0,0 +1,24 @@
+// inital state of app
+export const initalState = {
+    user: null,
+    playlist: [],
+    playing: false,
+    item: null
+}
+
+const reducer = (state,action) => {
+    console.log(action)
+    // action => action.type && [action.payload]
+    switch(action.type){
+        case "SET_USER":
+            return{
+                ...state,
+                user: action.user
+            }
+        default:
+            return state 
+    }
+
+}
+
+export default reducer;
